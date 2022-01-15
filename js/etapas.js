377f44aeb9b631949d0cb17dc503e34b8abb4e13
Lucas Carvalho <lucas.pereiracarvalho@hotmail.com>
2022-01-15 09:17:33 -0300

    primeiro-commit

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '30300',
+                nome: 'Cinthia Rangel',
+                partido: 'PFJ',
+                fotos:[
+                    {url: 'vereador1.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '40300',
+                nome: 'Manoel Carlos',
+                partido: 'PDP',
+                fotos:[
+                    {url: 'vereador2.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '55',
+                nome: 'José Carlos',
+                partido: 'PPEA',
+                vice: 'Janaina Silva',
+                fotos: [
+                    {url: 'prefeito1.png', legenda: 'Prefeito'},
+                    {url: 'vice1.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '98',
+                nome: 'Maria Abreu',
+                partido: 'PDBP',
+                vice: 'Cláudio Santos',
+                fotos: [
+                    {url: 'prefeito2.png', legenda: 'Prefeito'},
+                    {url: 'vice2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
